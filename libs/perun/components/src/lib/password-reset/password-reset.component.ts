0d0824b57c2ae051e0f4a320f5e6969bc18ba46f
Martin Kostka <mkostka62@gmail.com>
2021-09-03 13:12:02 +0200

    feat(user-profile): change password
    * added option to change password on selected namespace(if supported)
    * added shared dialog for changing the password

--- libs/perun/components/src/lib/password-reset/password-reset.component.ts
+++ libs/perun/components/src/lib/password-reset/password-reset.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Attribute, AttributesManagerService} from '@perun-web-apps/perun/openapi';
 import { StoreService } from '@perun-web-apps/perun/services';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { getDefaultDialogConfig } from '@perun-web-apps/perun/utils';
+import { ChangePasswordDialogComponent } from '../../../../dialogs/src/lib/change-password-dialog/change-password-dialog.component';
 
 @Component({
   selector: 'perun-web-apps-password-reset',
@@ -15,11 +18,12 @@ export class PasswordResetComponent implements OnInit {
   nameSpaces: string[] = [];
   logins: Attribute[] = [];
 
-  displayedColumns: string[] = ['namespace', 'value', 'change'];
+  displayedColumns: string[] = ['namespace', 'value', 'reset', 'change'];
   dataSource: MatTableDataSource<Attribute>;
 
   constructor(private attributesManagerService: AttributesManagerService,
-              private store: StoreService) {
+              private store: StoreService,
+              private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -36,8 +40,19 @@ export class PasswordResetComponent implements OnInit {
     });
   }
 
-  changePassword(login: string) {
+  resetPassword(login: string) {
     const url = this.store.get('pwd_reset_base_url');
     location.href = `${url}?login-namespace=${login}`;
+  }
+
+  changePassword(login){
+    const config = getDefaultDialogConfig();
+    config.width = '600px';
+    config.data = {
+      login: login.value,
+      namespace: login.friendlyName.split(':')[1]
+    };
+
+   this.dialog.open(ChangePasswordDialogComponent, config);
   }
 }
