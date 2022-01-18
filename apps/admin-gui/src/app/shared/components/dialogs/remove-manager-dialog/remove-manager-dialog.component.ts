9a0136168a4e482cfbd70004ae9dd27874e56144
xkureck <luckureckova@gmail.com>
2022-01-18 16:21:21 +0100

    fix(admin): when user removes himself as manager the authorization is checked
    * added alert when user is removing himself as manager

--- apps/admin-gui/src/app/shared/components/dialogs/remove-manager-dialog/remove-manager-dialog.component.ts
+++ apps/admin-gui/src/app/shared/components/dialogs/remove-manager-dialog/remove-manager-dialog.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
-import {NotificatorService} from '@perun-web-apps/perun/services';
+import { GuiAuthResolver, NotificatorService, StoreService } from '@perun-web-apps/perun/services';
 import {TranslateService} from '@ngx-translate/core';
 import { AuthzResolverService, Facility, Group, RichUser, Vo } from '@perun-web-apps/perun/openapi';
 import { Role } from '@perun-web-apps/perun/models';
@@ -24,7 +24,9 @@ export class RemoveManagerDialogComponent implements OnInit {
               @Inject(MAT_DIALOG_DATA) public data: RemoveManagerDialogData,
               private notificator: NotificatorService,
               private translate: TranslateService,
-              private authzService: AuthzResolverService) {
+              private authzService: AuthzResolverService,
+              private store: StoreService,
+              private authService: GuiAuthResolver) {
   }
 
   displayedColumns: string[] = ['name'];
@@ -32,10 +34,12 @@ export class RemoveManagerDialogComponent implements OnInit {
 
   loading: boolean;
   theme: string;
+  removeSelf: boolean;
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource<RichUser>(this.data.managers);
     this.theme = this.data.theme;
+    this.removeSelf = this.data.managers.map(user => user.id).includes(this.store.getPerunPrincipal().userId) && !this.authService.isPerunAdmin();
   }
 
   onCancel() {
