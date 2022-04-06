8e7506ec9679fa821d7ae59e1c61c9765a2dcdf9
xflord <493294@mail.muni.cz>
2022-04-06 11:28:49 +0200

    fix: redirect from login page to home if user logged in
    * when user is logged in and goes to /login, the login component isn't displayed and the user is redirected to /home instead

--- libs/perun/login/src/lib/login-screen/login-screen.component.ts
+++ libs/perun/login/src/lib/login-screen/login-screen.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@perun-web-apps/perun/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'perun-web-apps-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss'],
 })
-export class LoginScreenComponent {
-  constructor(private auth: AuthService) {}
+export class LoginScreenComponent implements OnInit {
+  constructor(private auth: AuthService, private router: Router) {}
 
+  ngOnInit() {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['/home']);
+    }
+  }
   startAuth(): void {
     this.auth.startAuthentication();
   }
