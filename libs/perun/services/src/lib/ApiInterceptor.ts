25ea2bba870d4dbb7e6afd46937e536c576ce1da
David Flor <493294@mail.muni.cz>
2022-11-01 13:59:11 +0100

    feat(admin): redirect back to correct page after login on session expiration

--- libs/perun/services/src/lib/ApiInterceptor.ts
+++ libs/perun/services/src/lib/ApiInterceptor.ts
@@ -60,7 +60,7 @@ export class ApiInterceptor implements HttpInterceptor {
         finalize(() => (this.dialogRefSessionExpiration = undefined));
         sessionStorage.setItem('auth:redirect', location.pathname);
         sessionStorage.setItem('auth:queryParams', location.search.substring(1));
-        this.authService.startAuthentication();
+        void this.initAuthService.handleAuthStart();
       });
     }
     // Apply the headers
