f5e1b7f887ea054ac3d90268833056a32c5c071b
Johana Supíková <johana.supikova@seznam.cz>
2022-12-21 08:20:04 +0100

    fix(admin): fix pagination label for estimated number of results

--- libs/perun/services/src/lib/custom-mat-paginator.ts
+++ libs/perun/services/src/lib/custom-mat-paginator.ts
@@ -5,8 +5,12 @@ import { MatPaginatorIntl } from '@angular/material/paginator';
 export class CustomMatPaginator extends MatPaginatorIntl {
   getRangeLabel = function (page: number, pageSize: number, length: number): string {
     let estimatedLength: string;
+    const start = page * pageSize + 1;
+    const range = (page + 1) * pageSize;
+    const end = range > length ? length : range;
+
     if (length < 1000) {
-      estimatedLength = String(length);
+      return `${start} – ${end} of ${String(length)}`;
     } else if (length < 10000) {
       estimatedLength = '1 000';
     } else if (length < 100000) {
@@ -14,8 +18,7 @@ export class CustomMatPaginator extends MatPaginatorIntl {
     } else {
       estimatedLength = '100 000';
     }
-    const start = page * pageSize + 1;
-    const end = (page + 1) * pageSize;
+
     return `${start} – ${end} of ${estimatedLength} +`;
   };
 }
