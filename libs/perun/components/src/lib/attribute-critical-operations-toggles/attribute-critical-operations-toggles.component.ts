63e5ff9d2c69ed2efe5fd8abf631b6f6aec4c35b
David Flor <493294@mail.muni.cz>
2023-01-12 13:08:45 +0100

    fix(admin): write operation are now correctly critical by default when creating new attribute definition

--- libs/perun/components/src/lib/attribute-critical-operations-toggles/attribute-critical-operations-toggles.component.ts
+++ libs/perun/components/src/lib/attribute-critical-operations-toggles/attribute-critical-operations-toggles.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class AttributeCriticalOperationsTogglesComponent {
   @Input() readOperation = false;
-  @Input() writeOperation = false;
+  @Input() writeOperation = true;
   @Output() readOperationChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() writeOperationChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 }
